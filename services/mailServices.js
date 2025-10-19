ff03d6c837bcff9337b90881239fc63925f656c7
Himanshu Nishad <himanshunishad620@gmail.com>
2025-10-19 14:38:23 +0530

    email check

--- services/mailServices.js
+++ services/mailServices.js
@@ -1,24 +1,42 @@
-const nodemailer = require("nodemailer");
-require("dotenv").config();
+const sgMail = require("@sendgrid/mail");
+const API_KEY =
+  "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";
+sgMail.setApiKey(API_KEY);
 const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    text,
+  const message = {
+    to: to,
+    from: "himanshunishad620@gmail.com",
+    subject: subject,
+    text: text,
   };
-
-  await transporter.sendMail(mailOptions);
+  sgMail
+    .send(message)
+    .then((res) => console.log("Email Sent"))
+    .catch((err) => console.log(err));
 };
 
 module.exports = sendEmail;
+// const nodemailer = require("nodemailer");
+// require("dotenv").config();
+// const sendEmail = async (to, subject, text) => {
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: process.env.EMAIL_USER,
+//     to,
+//     subject,
+//     text,
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendEmail;
