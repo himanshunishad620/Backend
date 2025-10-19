0123072fe8a7da121ca8982427ff51bd6f8159f4
Himanshu Nishad <himanshunishad620@gmail.com>
2025-10-19 14:48:51 +0530

    Sendgrid added

--- services/mailServices.js
+++ services/mailServices.js
@@ -1,13 +1,14 @@
 const sgMail = require("@sendgrid/mail");
+require("dotenv").config();
 const API_KEY =
   "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";
 sgMail.setApiKey(API_KEY);
 const sendEmail = async (to, subject, text) => {
   const message = {
-    to: to,
-    from: "himanshunishad620@gmail.com",
-    subject: subject,
-    text: text,
+    to,
+    from: process.env.EMAIL_USER,
+    subject,
+    text,
   };
   sgMail
     .send(message)
